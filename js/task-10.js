bc79da9feae8fb6794198c358227205d285dddc1
VasylMolnar <108887823+VasylMolnar@users.noreply.github.com>
2022-08-22 09:28:57 +0300

    Update task-10.js

--- js/task-10.js
+++ js/task-10.js
@@ -1,3 +1,31 @@
 function getRandomHexColor() {
     return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
 }
+
+const input = document.querySelector('[type="number"]');
+const btnCreate = document.querySelector('[data-create]');
+const btnDestroy = document.querySelector('[data-destroy]');
+const boxes = document.querySelector('#boxes');
+
+btnDestroy.addEventListener('click', () => {
+    boxes.innerHTML = '';
+});
+
+btnCreate.addEventListener('click', () => {
+    const amount = input.value;
+    const items = [];
+
+    for (let i = 1; i <= amount; i++) {
+        const createDiv = document.createElement('div');
+
+        createDiv.style.backgroundColor = getRandomHexColor();
+        createDiv.style.width = i * 10 + 30 + 'px';
+        createDiv.style.height = i * 10 + 30 + 'px';
+
+        items.push(createDiv);
+    }
+
+    boxes.append(...items);
+
+    input.value = '';
+});
